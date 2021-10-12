b2568ddf4425d146994ccc8f34486d92e5783818
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2021-10-12 22:14:00 +0200

    Update kom.cpp

--- IT WEEK/kom.cpp
+++ IT WEEK/kom.cpp
@@ -4,13 +4,13 @@ int mincost(int s, int d, bool visited[], int graph[100][100], int V)
 {
     if (s == d) return 0;
     visited[s] = 1;
-    int ans = 2137;
+    int ans = INT_MAX;
     for (int i = 0; i < V; i++)
     {
-        if (graph[s][i] != 2137 && !visited[i])
+        if (graph[s][i] != INT_MAX && !visited[i])
         {
             int curr = mincost(i, d, visited, graph, V);
-            if (curr < 2137) ans = min(ans, graph[s][i] + curr);
+            if (curr < INT_MAX) ans = min(ans, graph[s][i] + curr);
         }
     }
     visited[s] = 0;
@@ -22,7 +22,7 @@ int main()
     cin >> V >> E;
     int graph[100][100];
     bool visited[100] = { 0 };
-    for (int i = 0; i < V; i++) for (int j = 0; j < V; j++) graph[i][j] = 2137;
+    for (int i = 0; i < V; i++) for (int j = 0; j < V; j++) graph[i][j] = INT_MAX;
     for (int i = 0; i < E; i++)
     {
         cin >> a >> b >> w;
@@ -36,6 +36,6 @@ int main()
     else if(res <= 10) cout << 10;
     else if(res <= 20) cout << 20;
     else if(res <= 40) cout << 40;
-    else if(res > 40) cout << "IDZ  PIECHOTA.  NIE  MASZ PIENIEDZY";
+    else cout << "IDZ  PIECHOTA.  NIE  MASZ PIENIEDZY";
     return 0;
 }
